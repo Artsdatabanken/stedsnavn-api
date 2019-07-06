c919ccb3a73e500d078d3b9d7bd1e0a776b0a3b2
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-07-06 09:18:39 +0200

    Radius based search

--- src/quadtree.js
+++ src/quadtree.js
@@ -17,6 +17,51 @@ function getChild(tree, x, y) {
 
 let max = 0;
 
+function find2(quad, x, y, radius, z) {
+  const best = { distSquared: 9e9 };
+  find2_(quad, best, x, y, radius, z);
+  best.dist = Math.sqrt(best.distSquared) * Math.pow(0.5, z);
+  return best;
+}
+
+function distanceFromQuadSquared(px, py) {
+  const x = 0.5;
+  const y = 0.5;
+  const width = 1;
+  const height = 1;
+  dx = Math.max(Math.abs(px - x) - width / 2, 0);
+  dy = Math.max(Math.abs(py - y) - height / 2, 0);
+  return dx * dx + dy * dy;
+}
+
+function find2_(quad, best, x, y, radius, z) {
+  if (!quad) return;
+  false &&
+    console.log(
+      quad.value,
+      x,
+      y,
+      radius,
+      distanceFromQuadSquared(x, y),
+      radius * radius
+    );
+  if (distanceFromQuadSquared(x, y) > radius * radius) return;
+  if (z > 0) {
+    find2_(quad.nw, best, 2 * x, 2 * y, 2 * radius, z - 1);
+    find2_(quad.ne, best, 2 * (x - 0.5), 2 * y, 2 * radius, z - 1);
+    find2_(quad.sw, best, 2 * x, 2 * (y - 0.5), 2 * radius, z - 1);
+    find2_(quad.se, best, 2 * (x - 0.5), 2 * (y - 0.5), 2 * radius, z - 1);
+    return;
+  }
+  if (!quad.value) return;
+  const distSquared = (x - 0.5) * (x - 0.5) + (y - 0.5) * (y - 0.5);
+  console.log(quad.value + ": " + Math.sqrt(distSquared));
+  if (distSquared < best.distSquared) {
+    best.distSquared = distSquared;
+    best.value = quad.value;
+  }
+}
+
 function find(tree, x, y, z) {
   if (z === 0) return { [z]: tree.value };
   const leaf = getChild(tree, x, y);
@@ -42,4 +87,4 @@ function add(tree, x, y, z, value) {
   } else tree.value = value;
 }
 
-module.exports = { add, find };
+module.exports = { add, find, find2 };
--- src/quadtree.test.js
+++ src/quadtree.test.js
@@ -7,6 +7,44 @@ test("quadtile add", async () => {
   qt.add(root, 0.25, 0.25, 1, "B");
   qt.add(root, 0.25, 0.25, 2, "C");
 
-  console.log(JSON.stringify(root));
   expect(root).toMatchSnapshot();
+});
+
+test("quadtile find", async () => {
+  const root = {};
+
+  qt.add(root, 0, 0, 0, "A");
+  qt.add(root, 0.75, 0.75, 1, "B");
+  const actual = qt.find2(root, 0.25, 0.75, 0.25, 1);
+  expect(actual).toMatchSnapshot();
+});
+
+test("quadtile find out of range", async () => {
+  const root = {};
+
+  qt.add(root, 0, 0, 0, "A");
+  qt.add(root, 0.75, 0.75, 1, "B");
+  const actual = qt.find2(root, 0.25, 0.75, 0.24, 1);
+  expect(actual).toMatchSnapshot();
+});
+
+test("quadtile find root", async () => {
+  const root = {};
+
+  qt.add(root, 0, 0, 0, "A");
+  qt.add(root, 0.75, 0.75, 1, "B");
+  const actual = qt.find2(root, 0.25, 0.75, 0.24, 0);
+  expect(actual).toMatchSnapshot();
+});
+
+test("quadtile find multi", async () => {
+  const root = {};
+
+  qt.add(root, 0.1, 0.1, 2, "A");
+  qt.add(root, 0.9, 0.1, 2, "B");
+  qt.add(root, 0.1, 0.9, 2, "C");
+  qt.add(root, 0.9, 0.9, 2, "D");
+  debugger;
+  const actual = qt.find2(root, 0.51, 0.75, 0.5, 2);
+  expect(actual).toMatchSnapshot();
 });
