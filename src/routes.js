fe1c30ec72f765c5d0566285b6c466891606310e
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-08-26 00:06:36 +0200

    Fix: Assignment to constant var

--- src/routes.js
+++ src/routes.js
@@ -5,8 +5,8 @@ module.exports = function(app, index) {
     res.send('Try <a href="/17.07442,68.36028/32">/17.07442/68.36028/24</a>');
   });
   app.get("/:lon,:lat/:zoom?", (req, res) => {
-    const { lat, lon, zoom } = req.params;
-    if (!zoom) zoom = 24;
+    var { lat, lon, zoom } = req.params;
+    if (!zoom) zoom = 55;
     const r = api.lookup(index, lon, lat, zoom);
     res.setHeader("Content-Type", "application/json");
     res.send(JSON.stringify(r));
