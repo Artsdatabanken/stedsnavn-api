46caae47e794d9223ac79b0fe1ad187f1c479491
Bjorn Reppen <bjornreppen@users.noreply.github.com>
2019-11-18 12:06:37 +0100

    Rename parameter lon -> lng

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 const geometry = require("./geometry");
 const quadtree = require("./quadtree");
 
-function lookup(index, lon, lat, zoom) {
-  const coords = geometry.normalize([lon, lat], index.index.bounds);
+function lookup(index, lng, lat, zoom) {
+  const coords = geometry.normalize([lng, lat], index.index.bounds);
   const hit = quadtree.find2(index.index, coords[0], coords[1], zoom);
   if (hit.value) {
     const fields = hit.value.split(";");
--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,9 @@ const api = require("./api");
 
 module.exports = function(app, index) {
   app.get("/v1/punkt", (req, res) => {
-    var { lat, lon, zoom } = req.query;
+    var { lat, lng, zoom } = req.query;
     if (!zoom) zoom = 55;
-    const r = api.lookup(index, lon, lat, zoom);
+    const r = api.lookup(index, lng, lat, zoom);
     res.setHeader("Content-Type", "application/json");
     res.send(JSON.stringify(r));
   });
